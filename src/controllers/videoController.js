e08331113a88c68d9564795a5e580aca0d880160
wonjongseo <visionwill@naver.com>
2022-05-08 22:54:59 +0900

    edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,24 +10,46 @@ export const watch = async (req, res) => {
 
     const video = await Video.findById(id);
 
+    if (!video) {
+        return res.render("404", {pageTitle: "Video Not Found"});
+    }
     return res.render("watch", {
         pageTitle: video.title,
         video,
     });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const {id} = req.params;
-    const video = videos[id - 1];
 
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`});
+    const video = await Video.findById(id);
+
+    if (!video) {
+        return res.render("404", {pageTitle: "Video Not Found"});
+    }
+    return res.render("edit", {
+        pageTitle: `Editing ${video.title}`,
+        video,
+    });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const {id} = req.params;
-    const {title} = req.body;
+    const {title, description, hashtags} = req.body;
 
-    videos[id - 1].title = title;
+    const ok = await Video.exists({_id: id});
+
+    if (!ok) {
+        return res.render("404", {pageTitle: "Video Not Found"});
+    }
+
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+
     return res.redirect(`/videos/${id}`);
 };
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import "./models/Video";
 import app from "./server.js";
 
-const PORT = 4012;
+const PORT = 4013;
 
 const handleListening = () =>
     console.log(`✅ Server listening on http://localhost:${PORT}`);
